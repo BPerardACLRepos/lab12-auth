6544d0e2c62dc1d2a9076b4ade4250f6791bfbb1
Brandon Perard <brandon.perard@gmail>
2021-03-02 08:11:01 -0800

    add tests for GET/PUT/POST/DELETE user auth endpoints

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,57 +9,155 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns todos', async () => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+          id: 1,
+          todo: `Start Fire`,
+          completed: true,
+          user_id: 1,
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+          id: 2,
+          todo: `Extinguish Fire`,
+          completed: false,
+          user_id: 1,
         },
-        {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
-        }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/todos')
         .expect('Content-Type', /json/)
         .expect(200);
 
       expect(data.body).toEqual(expectation);
+    });
+
+    test('posts new todo from auth user enpoint', async () => {
+
+      const newTodo = {
+        todo: `Dance like nobody is watching`,
+        completed: false,
+      };
+
+      const expectation = [
+        {
+          ...newTodo,
+          id: 3,
+          user_id: 2,
+        }];
+
+      const data = await fakeRequest(app)
+        .post('/api/todos')
+        .send(newTodo)
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('updates todo from auth user enpoint', async () => {
+
+      const updatedTodo = {
+        todo: `Dance like nobody is watching`,
+        completed: true,
+      };
+
+      const expectation = [
+        {
+          ...updatedTodo,
+          id: 3,
+          user_id: 2,
+        }];
+
+      const data = await fakeRequest(app)
+        .put('/api/todos/3')
+        .send(updatedTodo)
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('returns todo from auth user enpoint', async () => {
+
+      const onlyTodo = {
+        todo: `Dance like nobody is watching`,
+        completed: true,
+      };
+
+      const expectation = [
+        {
+          ...onlyTodo,
+          id: 3,
+          user_id: 2,
+        }];
+
+      const data = await fakeRequest(app)
+        .get('/api/todos/')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('deletes todo from auth user enpoint', async () => {
+
+      const deletedTodo = {
+        todo: `Dance like nobody is watching`,
+        completed: true,
+      };
+
+      const expectation = [
+        {
+          ...deletedTodo,
+          id: 3,
+          user_id: 2,
+        }];
+
+      const dataDelete = await fakeRequest(app)
+        .delete('/api/todos/3')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(dataDelete.body).toEqual(expectation);
+
+      const expectationBlank = [];
+
+      const data = await fakeRequest(app)
+        .get('/api/todos/')
+        .set('Authorization', token)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectationBlank);
     });
   });
 });
